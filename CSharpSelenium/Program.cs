edce0fb99fb864cd802b7db1c5c21d35b2968f84
akheelassadi <akhlasdi23@gmail.com>
2022-08-21 20:42:36 +0530

    Getting started with automation framwerok set up

---
+++ CSharpSelenium/Program.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace CSharpSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver webDriver = new ChromeDriver("D://Learning//chrome_driver//");
+
+            webDriver.Url = "https://www.amazon.in/";
+        }
+    }
+}
